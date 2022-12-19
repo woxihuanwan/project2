7ed03caad9f65b6f4803812581c6064264384c4d
woxihuanwan <2580958711@qq.com>
2022-12-19 12:09:31 +0800

    这是第十周的项目提交

--- ch10/js/ez.js
+++ ch10/js/ez.js
@@ -2,7 +2,6 @@
  * 产品详情列表交互js
  */
 $(function() {
-    // 独家提供
     $('.ez-banner').tyslide({
             boxh: 460, //盒子的高度
             w: 1200, //盒子的宽度
@@ -27,5 +26,40 @@ $(function() {
         var index = $(this).index();
         //显示对应内容
         $('.ez-banner').eq(index).show().siblings().hide()
+    })
+
+
+    // 换一批
+    // // 上下
+    // var index = 0; //先写
+    // $('.change').click(function() {
+    //     //定一个索引
+    //     //自增
+    //     index++;
+    //     //边界判断
+    //     index = index > 2 ? 0 : index;
+    //     //动画效果 让inner-box运动
+    //     $('.inner-box').animate({
+    //         top: -index * 416
+    //     })
+    // })
+
+    // 左右滑动的版本
+    var index = 0; //先写
+    $('.change').click(function() {
+        //定一个索引
+        //自增
+        index++;
+        //边界判断
+        // index = index > 3 ? 0 : index;
+        //动画效果 让inner-box运动
+        $('.inner-box').animate({
+            left: -index * 1200
+        }, function() {
+            if (index === 3) {
+                index = 0;
+                $('.inner-box').css('left', 0)
+            }
+        })
     })
 })
